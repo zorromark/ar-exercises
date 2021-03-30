d34eea20ba145efa4dd57f918f5109c2da99d72b
Anita Toth <haluska99@hotmail.com>
2021-03-30 00:30:47 +0000

    complete exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,11 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+Store.create([
+  { name: 'Burnaby',  annual_revenue: 300000,  mens_apparel: true,  womens_apparel: true},
+  { name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true},
+  { name: 'Gastown',  annual_revenue: 190000,  mens_apparel: true,  womens_apparel: false}
+])
+
+puts Store.count
